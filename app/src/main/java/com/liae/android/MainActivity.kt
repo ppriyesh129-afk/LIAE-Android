b4ed3ebc29fe45175a220ef82438053ebc4f49e6
ppriyesh129-afk <ppriyesh129@gmail.com>
2026-09-07 06:40:08 +0530

    Implement MainActivity with ONNX Runtime message

--- app/src/main/java/com/liae/android/MainActivity.kt
+++ app/src/main/java/com/liae/android/MainActivity.kt
@@ -1,1 +1,16 @@
+package com.liae.android
 
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "LIAE Android\nONNX Runtime Ready"
+        tv.textSize = 24f
+        setContentView(tv)
+    }
+}
