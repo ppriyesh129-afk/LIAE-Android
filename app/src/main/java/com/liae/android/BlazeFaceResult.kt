be767789aefea41398a84513a434afd9e8b4466f
ppriyesh129-afk <ppriyesh129@gmail.com>
2026-09-07 16:26:32 +0530

    Add keypoints property to BlazeFaceResult
    Added keypoints property to store BlazeFace landmarks.

--- app/src/main/java/com/liae/android/BlazeFaceResult.kt
+++ app/src/main/java/com/liae/android/BlazeFaceResult.kt
@@ -5,5 +5,14 @@ data class BlazeFaceResult(
     val top: Float,
     val right: Float,
     val bottom: Float,
-    val score: Float
+    val score: Float,
+
+    // BlazeFace landmarks:
+    // 0 = right eye
+    // 1 = left eye
+    // 2 = nose
+    // 3 = mouth
+    // 4 = right ear
+    // 5 = left ear
+    val keypoints: Array<FloatArray> = emptyArray()
 )
